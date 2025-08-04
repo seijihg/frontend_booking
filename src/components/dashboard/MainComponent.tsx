a7589fd4f6c68b641b08b245358a578732a2ed19
Le H Ngo <hgle.ngo@gmail.com>
2025-08-05 00:01:11 +0100

    Refactor checking user to server side dashboard

--- src/components/dashboard/MainComponent.tsx
+++ src/components/dashboard/MainComponent.tsx
@@ -1,28 +1,14 @@
 "use client";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 import CalendarView from "@/components/calendar/CalendarView";
 import NavBar from "@/components/dashboard/navigation_bar/NavBar";
-import { useUserStore } from "@/stores/userStore";
 import DashboardView from "../profile/DashboardView";
 
 type ViewType = "dashboard" | "calendar" | "team" | "projects";
 
 export default function MainComponent() {
-  const router = useRouter();
-  const { user } = useUserStore();
-  const [isLoading, setIsLoading] = useState(false); //change to true if useEffect
   const [currentView, setCurrentView] = useState<ViewType>("dashboard");
-
-  // useEffect(() => {
-  //   console.log(user);
-  //   if (!user) {
-  //     router.push("/login");
-  //   } else {
-  //     setIsLoading(false);
-  //   }
-  // }, [user, router]);
 
   const renderContent = () => {
     switch (currentView) {
@@ -44,11 +30,9 @@ export default function MainComponent() {
   };
 
   return (
-    !isLoading && (
-      <div className="flex h-screen flex-col">
-        <NavBar currentView={currentView} onViewChange={setCurrentView} />
-        <div className="flex-1 overflow-auto">{renderContent()}</div>
-      </div>
-    )
+    <div className="flex h-screen flex-col">
+      <NavBar currentView={currentView} onViewChange={setCurrentView} />
+      <div className="flex-1 overflow-auto">{renderContent()}</div>
+    </div>
   );
 }
